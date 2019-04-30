c2ad67c65d32a4652f79aaede227ec571fd9ace3
Filipe Oliveira <filipe.soares@ans.gov.br>
2019-04-30 14:55:35 -0300

    Remove route in index.js

--- index.js
+++ index.js
@@ -3,15 +3,3 @@ const app = require('./src/config/server');
 app.listen(3000, function () {
     console.log('server running...');
 });
-
-app.get('/', function (req, res) {
-    res.send(
-        `<html>
-            <head>
-                <title>::Cataloger::</title>
-            </head>
-            <body>
-                <p>Cataloger</p>
-            </body>
-        </html>`);
-});
